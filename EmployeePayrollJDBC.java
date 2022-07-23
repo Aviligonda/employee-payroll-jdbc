1196414e3fef0301fb1e04838b632930495d3490
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-23 17:28:03 +0530

    ADDED : DataBase connect to java sucessfully

--- EmployeePayrollJDBC.java
+++ EmployeePayrollJDBC.java
@@ -1,8 +1,20 @@
 package com.bridzelab;
 
-public class EmployeePayrollJDBC {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Payroll program on JDBC");
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class EmployeePayrollJDBC {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Welcome to Employee Payroll program on JDBC");
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root", "Sravani@6086");
+            System.out.println("Connection done.....");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
     }
 }
